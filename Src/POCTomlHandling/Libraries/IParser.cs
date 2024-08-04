7168fa135185770ecdc80d442588ebb3a486cce0
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-08-04 02:32:40 +0100

    Add Tomlyn

---
+++ Src/POCTomlHandling/Libraries/IParser.cs
@@ -0,0 +1,6 @@
+﻿namespace POCTomlHandling.Libraries;
+
+internal interface IParser
+{
+    object ParseTomlString(string content, string root, string key);
+}
---
+++ Src/POCTomlHandling/Libraries/TommyParser.cs
@@ -0,0 +1,13 @@
+﻿using Tommy;
+
+namespace POCTomlHandling.Libraries;
+
+public class TommyParser : IParser
+{
+    public object ParseTomlString(string content, string root, string key)
+    {
+        using var reader = new StringReader(content);
+        var table = TOML.Parse(reader);
+        return table[root][key].AsString.Value;
+    }
+}
